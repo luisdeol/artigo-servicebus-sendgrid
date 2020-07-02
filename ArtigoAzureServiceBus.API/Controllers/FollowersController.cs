16ebab8534f35acf728a162f723b79dbce885931
luisfelipe <luisdeol@hotmail.com>
2020-07-02 16:47:23 -0300

    Parte 1

---
+++ ArtigoAzureServiceBus.API/Controllers/FollowersController.cs
@@ -0,0 +1,41 @@
+﻿using ArtigoAzureServiceBus.API.InputModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtigoAzureServiceBus.API.Controllers
+{
+    [Route("api/{controller}")]
+    public class FollowersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly QueueClient _queueClient;
+        private const string QUEUE_NAME = "email-queue";
+
+        public FollowersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            var connectionString = _configuration.GetSection("ServiceBus:ConnectionString").Value;
+
+            _queueClient = new QueueClient(connectionString, QUEUE_NAME);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Follow([FromBody] UserFollowingInputModel userFollowingInputModel)
+        {
+            var inputModelJsonString = JsonConvert.SerializeObject(userFollowingInputModel);
+
+            var messageBytes = Encoding.UTF8.GetBytes(inputModelJsonString);
+
+            var message = new Message(messageBytes);
+
+            await _queueClient.SendAsync(message);
+
+            return Ok();
+        }
+    }
+}
---
+++ ArtigoAzureServiceBus.API/InputModels/UserFollowingInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArtigoAzureServiceBus.API.InputModels
+{
+    public class UserFollowingInputModel
+    {
+        public int IdUserFollower { get; set; }
+        public int IdUserFollowee { get; set; }
+        public DateTime FollowedAt { get; set; } = DateTime.Now;
+        public string Email { get; set; } = "umemail@email.com";
+    }
+}
